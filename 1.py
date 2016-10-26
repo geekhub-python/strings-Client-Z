cb5d9ba5366d7e1d026a58ba33a1f2878eb6fee8
Denis <denisoleshenko@gmail.com>
2016-10-26 17:17:02 +0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+text = input('Enter the string: ')
+print("1 -", text[2])
+print("2 -", text[-2])
+print("3 -", text[5])
+print("4 -", text[:-2])
+print("5 -", len(text))
+print("6 -", text[::2])
+print("7 -", text[1::2])
+print("8 -", text[::-1])
+print("9 -", text[::-2])
